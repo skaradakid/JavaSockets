0ee6c319c5920b5a59b00131301c9e56cba3dba3
karabo <mawaiskara@gmail.com>
2025-04-20 22:59:56 +0200

    first push

---
+++ src/main/java/za/co/wethinkcode/person/Main.java
@@ -0,0 +1,7 @@
+package za.co.wethinkcode.person;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/za/co/wethinkcode/person/Server.java
@@ -0,0 +1,4 @@
+package za.co.wethinkcode.person;
+
+public class Server {
+}
