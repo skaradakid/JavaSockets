9f370e84856de49dec7d765ddee8d59171d455b4
karabo <mawaiskara@gmail.com>
2025-04-21 00:33:30 +0200

    listening to client

--- src/main/java/za/co/wethinkcode/person/Server.java
+++ src/main/java/za/co/wethinkcode/person/Server.java
@@ -1,5 +1,8 @@
 package za.co.wethinkcode.person;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 
@@ -10,6 +13,10 @@ public class Server {
             ServerSocket ss = new ServerSocket(5050);
             Socket soc = ss.accept();
             System.out.println("connection established");
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String str = in.readLine();
+            PrintWriter out  = new PrintWriter(soc.getOutputStream(), true);
+            out.println("Server says: "+ str);
         }
         catch (Exception e){
             e.printStackTrace();
