e6ec9820465c77d91401f80880b8581afe128802
karabo <mawaiskara@gmail.com>
2025-04-20 23:16:19 +0200

    added sever

--- src/main/java/za/co/wethinkcode/person/Server.java
+++ src/main/java/za/co/wethinkcode/person/Server.java
@@ -1,4 +1,18 @@
 package za.co.wethinkcode.person;
 
+import java.net.Socket;
+import java.net.ServerSocket;
+
 public class Server {
+    public static void main(String[] args){
+        System.out.println("waiting for clients!");
+        try {
+            ServerSocket ss = new ServerSocket(5050);
+            Socket soc = ss.accept();
+            System.out.println("connection established");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
